d94a9b481b35015322fda6b1a3b0310abf15c5bf
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-08-08 18:04:32 -0400

    Add @ColumnInfo to all columns

--- app/src/main/java/com/ndomx/reign/db/Post.kt
+++ app/src/main/java/com/ndomx/reign/db/Post.kt
@@ -7,9 +7,10 @@ import java.util.*
 
 @Entity(tableName = "posts")
 data class Post(
-    @PrimaryKey var id: Int,
-    var author: String,
+    @PrimaryKey
+    @ColumnInfo(name = "id") var id: Int,
+    @ColumnInfo(name = "author") var author: String,
     @ColumnInfo(name = "created_at") var createDate: Date,
-    var url: String,
-    var title: String
+    @ColumnInfo(name = "url") var url: String,
+    @ColumnInfo(name = "title") var title: String
 )
